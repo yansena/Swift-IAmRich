785ff738cd5c46cbbcf561e270cbbdd481e4809c
Yan Sena <contato@yandev.com.br>
2022-08-16 17:43:13 -0300

    Upload all content

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Riich
+//
+//  Created by Yan Sena Dev on 3/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
